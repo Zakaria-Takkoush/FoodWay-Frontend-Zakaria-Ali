943052cbc4fa39c9453e417a6488ca638a0661e5
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-05-28 14:35:09 +0300

    Frontend - Added onclick on restaurant card move to restaurant page

--- pages/resto_page/resto_page.js
+++ pages/resto_page/resto_page.js
@@ -3,31 +3,32 @@ logo.addEventListener("click", function(){
     document.location='../landing_page/landing_page.html';
 })
 
-let slideIndex = 1;
-showSlides(slideIndex);
+
+let slide_index = 1;
+showSlides(slide_index);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(slide_index += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides(slide_index = n);
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {slide_index = 1}
+  if (n < 1) {slide_index = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slide_index-1].style.display = "block";
+  dots[slide_index-1].className += " active";
 }
