6eaf1cc6e4dc4a5293c10c490cc3bda00beac29a
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-05-28 22:43:23 +0300

    Frontend - Added the onclick function for Create Frontend - Added localStorage

--- pages/admin_restos/admin_restos.js
+++ pages/admin_restos/admin_restos.js
@@ -20,3 +20,29 @@ reviews_page.addEventListener("click", function () {
 })
 
 
+let resto_name = document.getElementById("resto_name");
+let phone_number = document.getElementById("phone_number");
+let cate = document.getElementById("cate");
+let city = document.getElementById("city");
+let description = document.getElementById("description");
+let new_restos=[];
+if (localStorage.restos != null) {
+    new_restos = JSON.parse(localStorage.restos);
+} else {
+    new_restos = [];
+}
+localStorage.setItem("restos", JSON.stringify(new_restos));
+let create = document.getElementById("create")
+create.addEventListener("click", function () {
+    let new_resto = {
+        "name": resto_name.value,
+        "phone_number": phone_number.value,
+        "cate": cate.value,
+        "city": city.value
+    }
+    new_restos.push(new_resto);
+    localStorage.setItem("restos", JSON.stringify(new_restos));
+    
+    console.log(new_restos);
+})
+
