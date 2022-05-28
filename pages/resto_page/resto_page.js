d64cea219fe035d36bce4a600c5d95addaf8401b
Zakaria <zaktakk96@gmail.com>
2022-05-28 16:17:43 +0300

    Frontend - Created "Add Review" pop-up

--- pages/resto_page/resto_page.js
+++ pages/resto_page/resto_page.js
@@ -32,3 +32,29 @@ function showSlides(n) {
   slides[slide_index-1].style.display = "block";
   dots[slide_index-1].className += " active";
 }
+
+
+// pop-up reviews section 
+// Get the modal
+var pop_up = document.getElementById("pop_up");
+
+// Get the button that opens the review
+var btn = document.getElementById("add_review");
+
+// Get the <span> element that closes the form
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the form
+btn.onclick = function() {
+  pop_up.style.display = "block";
+}
+// When the user clicks on <span> (x), close the pop_up
+span.onclick = function() {
+  pop_up.style.display = "none";
+}
+// When the user clicks anywhere outside of the pop_up, close it
+window.onclick = function(event) {
+  if (event.target == pop_up) {
+    pop_up.style.display = "none";
+  }
+}
