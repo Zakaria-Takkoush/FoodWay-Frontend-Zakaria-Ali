01de1f93940d37cfd1a2dba87a4cb7df5c5eef22
Zakaria <zaktakk96@gmail.com>
2022-05-30 20:34:04 +0300

    Frontend - Created signup api.js

---
+++ api.js
@@ -0,0 +1,24 @@
+let create_account = document.getElementById("sign_up_btn");
+
+create_account.addEventListener("click", function(event) {
+    event.preventDefault()
+    alert("Hiii")
+
+    /*
+    let data = new FormData();
+    data.append('user_id', id);
+    axios({
+        method: 'post',
+        url: 'http://localhost/project/get_restos.php',
+        data: data,
+    })
+    .then(function (response) {
+        console.log(response);
+        }
+    )
+    */
+
+})
+
+
+
