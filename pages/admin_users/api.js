4c89fd03c151eb096c075e702af0052130064669
Zakaria <zaktakk96@gmail.com>
2022-05-30 23:11:18 +0300

    Frontend - created admin_users api js and variables

--- pages/admin_users/api.js
+++ pages/admin_users/api.js
@@ -1,4 +1,4 @@
-/*
+
 let data = new FormData();
     axios({
         method: 'get',
@@ -6,7 +6,23 @@ let data = new FormData();
         data: data,
     })
     .then(function (response) {
+        users = response.data;
         console.log(response.data);
+       // console.log(response.data[0].first_name)
+       // console.log(response.data[1].user_id)
         }
     )
-*/
+
+const id = users = response.data[i].user_id
+const f_name = users = response.data[i].first_name
+const l_name = users = response.data[i].last_name
+const gender = users = response.data[i].gender
+const email = users = response.data[i].email
+const password = users = response.data[i].password
+const phone_number = users = response.data[i].phone_number
+const city_id = users = response.data[i].city_id
+const image = users = response.data[i].image
+const is_admin = users = response.data[i].is_admin
+
+// identify variables
+
