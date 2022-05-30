50f8bd0d5284437a9a7e808914878329624627ad
Zakaria <zaktakk96@gmail.com>
2022-05-30 21:02:06 +0300

    Frontend - Added Acios function to signup

--- api.js
+++ api.js
@@ -2,21 +2,28 @@ let create_account = document.getElementById("sign_up_btn");
 
 create_account.addEventListener("click", function(event) {
     event.preventDefault()
-    alert("Hiii")
 
-    /*
+    // params: first_name, last_name, gender, email, password, phone_number, city_id
+
+    //Axios Function - Post
+
     let data = new FormData();
-    data.append('user_id', id);
+    data.append('first_name', "");
+    data.append('last_name', "");
+    data.append('gender', "");
+    data.append('email', "");
+    data.append('password', "");
+    data.append('phone_number', "");
+    data.append('city_id', "");
     axios({
         method: 'post',
-        url: 'http://localhost/project/get_restos.php',
+        url: 'http://localhost/FoodWay-Backend/signup.php',
         data: data,
     })
     .then(function (response) {
         console.log(response);
         }
     )
-    */
 
 })
 
