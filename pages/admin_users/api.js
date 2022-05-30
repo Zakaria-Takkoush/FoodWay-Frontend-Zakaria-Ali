611e2d00be43cbd52aa355ffc5023876edc227e9
Zakaria <zaktakk96@gmail.com>
2022-05-30 22:57:00 +0300

    Frontend - Added "required" to login credentials

---
+++ pages/admin_users/api.js
@@ -0,0 +1,12 @@
+/*
+let data = new FormData();
+    axios({
+        method: 'get',
+        url: 'http://localhost/FoodWay-Backend/get_users.php',
+        data: data,
+    })
+    .then(function (response) {
+        console.log(response.data);
+        }
+    )
+*/
