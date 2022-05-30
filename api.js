0e903f5c58b56acc888a82373b2bcad025eabb87
Zakaria <zaktakk96@gmail.com>
2022-05-30 22:32:44 +0300

    Frontend - Linked signup to api

--- api.js
+++ api.js
@@ -5,23 +5,42 @@ create_account.addEventListener("click", function(event) {
 
     // params: first_name, last_name, gender, email, password, phone_number, city_id
 
+    const first_name = document.getElementById("fname").value 
+    const last_name = document.getElementById("lname").value
+    const gender = document.getElementById("gender").value
+ //   const gender_val = select.options[gender.selectedIndex].value;
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    const rep_password = document.getElementById("r_password").value
+    //const phone_number = document.getElementById("number").value
+    const city_id = document.getElementById("city").value
+
+    if (password !== rep_password) {
+        alert("Passwords do not match!")
+        return
+    }
+
     //Axios Function - Post
 
     let data = new FormData();
-    data.append('first_name', "");
-    data.append('last_name', "");
-    data.append('gender', "");
-    data.append('email', "");
-    data.append('password', "");
-    data.append('phone_number', "");
-    data.append('city_id', "");
+    data.append('first_name', first_name);
+    data.append('last_name', last_name);
+    data.append('gender', gender);
+    data.append('email', email);
+    data.append('password', password);
+    data.append('phone_number', '123');
+    data.append('city_id', city_id);
     axios({
         method: 'post',
         url: 'http://localhost/FoodWay-Backend/signup.php',
         data: data,
     })
     .then(function (response) {
-        console.log(response);
+        let result = response.data;
+        let message = result.status;
+        if (message === "Account Created!") {
+            document.getElementsByClassName("signup_top")[0].textContent = "Account Created!"
+        }
         }
     )
 
