0918024a7e3503daa675551e9be10277b00d266d
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-05-30 22:50:40 +0300

    Backend - Added comments

--- pages/landing_page/landing_page.js
+++ pages/landing_page/landing_page.js
@@ -1,9 +1,12 @@
+
+// On click of the resto Card go to the restaurant page
 let resto_card = document.getElementsByClassName("card");
 for (let i = 0; i < resto_card.length; i++) {
     resto_card[i].addEventListener("click", function () {
         document.location = "../resto_page/resto_page.html";
     })
 }
+// Just show the first row and on click of see all u can see all restos and on click on it again u can see the first row
 let see_all = document.getElementsByClassName("see-all-btn")[0];
 see_all.addEventListener("click", function () {
     for (let i = 0; i < resto_card.length; i++) {
@@ -12,9 +15,9 @@ see_all.addEventListener("click", function () {
 })
 
 
-let logoo = document.getElementsByClassName("header-logo")[0];
-let resto_cards = document.getElementById("restos-card");
 
+
+// Get all the restaurants in the database and show them in the landing page 
 window.addEventListener("load", function (event) {
 
     axios({
@@ -22,16 +25,10 @@ window.addEventListener("load", function (event) {
         url: 'http://localhost/FoodWay-Backend/restaurants.php',
     })
         .then(function (response) {
-            console.log(response.data);
+            
             let restaurants = response.data;
-            // let resto_id = restaurants[0].resto_id;
-
-            // let description = restaurants[0].description;
-            // let resto_img = restaurants[0].resto_img;
-            // let resto_city_id = restaurants[0].city_id;
-            // let resto_cat_id = restaurants[0].cat_id;
-
             let resto = '';
+            // Adding every resto in the database as the below div 
             for (let i = 0; i < restaurants.length; i++) {
                 let resto_name = restaurants[i].resto_name;
                 resto = `
@@ -44,9 +41,6 @@ window.addEventListener("load", function (event) {
             `
                 document.getElementById("resto-card").innerHTML += resto;
             }
-
-
-
         }
         )
 
