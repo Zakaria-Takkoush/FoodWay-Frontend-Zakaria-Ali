faa83830155150867f5a2a6842f86b35e8d70299
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-05-31 18:30:04 +0300

    Frontend - Edit restos Didn't work will get to it later

--- pages/admin_restos/api.js
+++ pages/admin_restos/api.js
@@ -1,17 +1,19 @@
 let add_resto = document.getElementById("create");
 let edit = document.getElementById("edit");
-let delete_resto= document.getElementById("delete");
+let delete_resto = document.getElementById("delete");
+let all_restaurants;
+ // params: resto_name,phone_number,city_id,desc,cat
+
+    let resto_name = document.getElementById("resto_name").value
+    let phone_number = document.getElementById("phone_number").value
+    let city_id = document.getElementById("city").value
+    let cat_id = document.getElementById("cate").value
+    let desc = document.getElementById("description").value
 
 add_resto.addEventListener("click", function (event) {
     event.preventDefault()
 
-    // params: resto_name,phone_number,city_id,desc,cat
-
-    const resto_name = document.getElementById("resto_name").value
-    const phone_number = document.getElementById("phone_number").value
-    const city_id = document.getElementById("city").value
-    const cat_id = document.getElementById("cate").value
-    const desc = document.getElementById("description").value
+   
 
     //Axios Function - Post
 
@@ -50,13 +52,10 @@ axios({
     .then(function (response) {
 
         let restaurants = response.data;
+        all_restaurants = restaurants;
         console.log(restaurants);
-        let resto = '';
         // Adding every resto in the database as the below div 
         for (let i = 0; i < restaurants.length; i++) {
-            let resto_name = restaurants[i].resto_name;
-            let resto_id = restaurants[i].resto_id;
-            let resto_city = restaurants[i].city_id;
             let table = '';
             for (let i = 0; i < restaurants.length; i++) {
                 table += `
@@ -67,18 +66,26 @@ axios({
                 <td>${restaurants[i].cat_id}</td>
                 <td>${restaurants[i].city_id}</td>
                 <td>${restaurants[i].description}</td>   
-                <td class="edit" onclick="updateData(${restaurants[i].resto_id})" id="edit" ><i class="fa-solid fa-pen-to-square"></i></td>
+                <td class="edit" onclick="editResto(${restaurants[i].resto_id})" id="edit" ><i class="fa-solid fa-pen-to-square"></i></td>
                 <td onclick="deleteResto(${restaurants[i].resto_id})" class="delete" id="delete"><i class="fa-solid fa-xmark"></i></td>
                 </tr>`;
             }
             document.getElementById("tbody").innerHTML = table;
+            edit.addEventListener("click", function () {
+            resto_name.value="restaurants[idd].resto_name";
+            phone_number.value="restaurants[idd].phone_number";
+            city_id.value="restaurants[idd].city_id";
+            cat_id.value="restaurants[idd].cat_id";
+            desc.value="restaurants[idd].description";
+        })
         }
+        
     }
     )
 
-    delete_resto.addEventListener("click", deleteResto());
+delete_resto.addEventListener("click", deleteResto());
 
-    function deleteResto(resto_id) {
+function deleteResto(resto_id) {
     let data = new FormData();
     data.append('id', resto_id);
     axios({
@@ -90,9 +97,40 @@ axios({
             let result = response.data;
             console.log(result);
         }
-        )}
+        )
+}
+
+// edit.addEventListener("click", editResto());
+
+// function editResto(id) {
+//     console.log("clicked")
+//     resto_name.value = all_restaurants[id].resto_name;
+//     phone_number.value = all_restaurants[id].phone_number;
+//     city_id.value = all_restaurants[id].city_id;
+//     cat_id.value = all_restaurants[id].cat_id;
+//     desc.value = all_restaurants[id].description;
+//   } 
 
 
 
 
+
+
+    // let data = new FormData();
+    // data.append('name', resto_name);
+    // data.append('phone', phone_number);
+    // data.append('description', desc);
+    // data.append('cat_id', cat_id);
+    // data.append('city_id', city_id);
+    // data.append('id', resto_id);
+    // axios({
+    //     method: 'post',
+    //     url: 'http://localhost/FoodWay-Backend/edit_resto.php',
+    //     data: data,
+    // })
+    //     .then(function (response) {
+    //         let result = response.data;
+    //         console.log(result);
+    //     })
+   
 
